d37fe27d0244319b7c85c40b9beb58210ec3c9d9
Khumoyun Inoyatov <khumoyun@inoyatov.com>
2019-03-18 23:33:47 +0500

    Fix: 2 Kata

--- 2_stop_gninnips_my_sdrow/stop_gninnips_my_sdrow.js
+++ 2_stop_gninnips_my_sdrow/stop_gninnips_my_sdrow.js
@@ -1,2 +1,15 @@
 const str = "Hello mellow yellow";
 
+function spinWords() {
+    const arr = str.split(' ');
+    arr.map((item) => {
+        if(item.length>=5) {
+            const final = item.split("").reverse().join("");
+            console.log(final);
+        } else {
+            return item;
+        }
+    })
+}
+
+spinWords();
